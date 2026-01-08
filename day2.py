671a634040494478fcf54c14c5e6bd719d342931
gatlaudayteja <gatlaudayteja1@gmail.com>
2026-01-08 16:52:32 +0530

    Add files via upload
    variables

---
+++ day2.py
@@ -0,0 +1,14 @@
+name = "uday" #string
+age = 20
+weight = 55.50
+list1 =[1,2,"4i",(10,20)]
+tuple1 = (10,20,30,"welcome","hello",50,60,70,80)
+set1 = {'A',2,3,4,5,6,7,8,9,10,'i','q','k',2,3}
+student_marks = {"maths":90,"telugu":98,"english":78}
+print("my name is",name)
+print("my age is",age)
+print("my weight is",weight)
+print("list of items",list1)
+print("my set is",set1)
+print("tuple is items",tuple1)
+print("student subject wise marks",student_marks)
